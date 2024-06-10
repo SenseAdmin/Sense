189bacbc6e59d56b10e6a18f7c2af7bb8c26fba3
SenseAdmin <senseapp@internet.ru>
2024-06-10 20:59:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,60 @@ document.getElementById("more-modal").addEventListener('click', event => {
     if (event._isClickInModal) return;
     event.currentTarget.classList.remove("open");
 });
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let savedImage = localStorage.getItem('savedImage');
+    if (savedImage) {
+        document.getElementById('uploadedImg').style.backgroundImage = `url(${savedImage})`;
+    } else {
+        document.getElementById('uploadedImg').style.backgroundImage = `url('img/alt-img.png')`;
+    }
+});
+
+document.getElementById('uploadBtn').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function() {
+    const file = this.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        document.getElementById('uploadedImg').style.backgroundImage = `url(${e.target.result})`;
+        localStorage.setItem('savedImage', e.target.result);
+    };
+
+    reader.readAsDataURL(file);
+});
+
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('intrest-label')) {
+        const inputId = event.target.getAttribute('for');
+        const input = document.getElementById(inputId);
+        
+        if (input) {
+            event.target.remove(); // Remove label
+            input.remove(); // Remove input
+            // Check if input has a pseudo-element .after and remove it
+            const inputStyle = window.getComputedStyle(input, ':after');
+            if (inputStyle.content !== 'none') {
+                input.style.content = 'none';
+            }
+        }
+        
+        const labels = document.querySelectorAll('.intrest-label');
+        if (labels.length === 0) {
+            const container = document.querySelector('.cont-intrest-index');
+            container.classList.add('off');
+        }
+    }
+});
